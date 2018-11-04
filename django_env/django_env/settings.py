bb7212b9bf33dc6c96628e0c4e906546195233d2
limengmeng <limengmeng004@ke.com>
2018-11-04 21:43:51 +0800

    修改数据库配置

--- django_env/django_env/settings.py
+++ django_env/django_env/settings.py
@@ -74,13 +74,21 @@ WSGI_APPLICATION = 'django_env.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '10.26.27.133',
+        'USER': 'root',
+        'PASSWORD': '123456',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
