0c2a59be617f4a8d6e7af12b91b892b6416bdec9
limengmeng <limengmeng004@ke.com>
2018-11-08 09:47:49 +0800

    DB测试

--- django_env/env/views.py
+++ django_env/env/views.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-
+from django_env.env.models import ResourceBase
+from django_env.env.models import ResourceEnvBase
+from django_env.env.models import ResourceJira
+from django_env.env.models import ResourceModuleArchive
+from django.http import HttpResponse
 
 # Create your views here.
 
-def test(request):
-    pass
+def testdb(request):
+    return HttpResponse("这是一个测试DB的函数。")
+
+
+
