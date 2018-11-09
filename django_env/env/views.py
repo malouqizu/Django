f1c941d60d252194dd5dc82e134114cae904c41d
limengmeng <limengmeng004@ke.com>
2018-11-09 10:14:42 +0800

    创建实例，实验所有表都能够插入正确的数据

--- django_env/env/views.py
+++ django_env/env/views.py
@@ -12,14 +12,23 @@ from common.logger import Log
 def testdb(request):
     resource_base = ResourceBase(base_type='redis', resource_data='i am resource_data', status=1, creator='lmm', is_valid=1)
     resource_base.save()
-
-    resource_base = ResourceBase(base_type='db', resource_data='i am resource_db', status=1, creator='lmm',
-                                 is_valid=1)
+    resource_base = ResourceBase(base_type='db', resource_data='i am resource_db', status=1, creator='lmm',is_valid=1)
+    resource_base.save()
+    resource_base = ResourceBase(base_type='kafka', resource_data='i am resource_kafka', status=1, creator='lmm',is_valid=1)
     resource_base.save()
 
-    resource_base = ResourceBase(base_type='kafka', resource_data='i am resource_kafka', status=1, creator='lmm',
-                                 is_valid=1)
-    resource_base.save()
+    resource_env_base = ResourceEnvBase(base_id=1, env='test3', ip='10.26.14.3', creator='lmm')
+    resource_env_base.save()
+
+    resource_jira = ResourceJira(jira_id=2365, jira_status=1, res_type='Redis', resource_id=2333,
+                                 submit_data='redis submit data', result_data='redis result data', creator='lmm')
+    resource_jira.save()
+
+    resource_module_archive = ResourceModuleArchive(archive_id='333', ip_flag='10.26.14.33', resource_host='10.26.14.33',
+                                                    resource_http_port='8000', module_domain='test3-i.bk-house-api.ke.com',
+                                                    https=1, nginx=1, namespace=0, extend_port_num=0, extend_data='/public',
+                                                    custom='need custom data', status=1)
+    resource_module_archive.save()
 
     Log.info("resource_base = ResourceBase(base_type='redis', resource_data='i am resource_data', status=1, creator='lmm', is_valid=1)")
     Log.info(type(resource_base))
