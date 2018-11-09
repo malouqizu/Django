cb1ab48aff060d6c4ee7e09c0c0b5c49a3141e12
limengmeng <limengmeng004@ke.com>
2018-11-09 13:35:01 +0800

    实验更新数据操作

--- django_env/env/views.py
+++ django_env/env/views.py
@@ -13,14 +13,11 @@ def testdb_add_method1(request):
     # 先创建对象实例，然后保存数据
     resource_base = ResourceBase(base_type='redis', resource_data='i am resource_data', status=1, creator='lmm', is_valid=1)
     resource_base.save()
-
     resource_env_base = ResourceEnvBase(base_id=1, env='test7', ip='10.26.14.7', creator='lmm')
     resource_env_base.save()
-
     resource_jira = ResourceJira(jira_id=2368, jira_status=1, res_type='Redis', resource_id=2338,
                                  submit_data='redis submit data', result_data='redis result data', creator='lmm')
     resource_jira.save()
-
     resource_module_archive = ResourceModuleArchive(archive_id='337', ip_flag='10.26.14.37', resource_host='10.26.14.37',
                                                     resource_http_port='8000', module_domain='test3-i.bk-house-api.ke.com',
                                                     https=1, nginx=1, namespace=0, extend_port_num=0, extend_data='/public',
@@ -40,7 +37,6 @@ def testdb_add_method2(request):
                          https=1, nginx=1, namespace=0, extend_port_num=0, extend_data='/public',
                          custom='need custom data', status=1)
     return HttpResponse("(创建对象，并同时保存对象)向数据库env各表中插入数据")
-
 
 def testdb_add_method3(request):
     # 在表中批量插入多条数据
@@ -66,4 +62,13 @@ def testdb_add_method3(request):
     ResourceModuleArchive.objects.bulk_create(ResourceModuleArchive_list_to_insert)
 
     return HttpResponse('批量插入多条数据')
+
+def testdb_update_method1(request):
+    # 更新一条数据，也只能更新一条数据
+    t = ResourceBase.objects.get(id=79)
+    t.creator = 'nick'
+    t.ip = '10.10.10.10'
+    t.save()
+
+    return HttpResponse('更新一条数据，也只能更新一条数据')
 
