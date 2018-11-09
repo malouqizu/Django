2af95e9ad675c4f011a13e0a9535a1e2cd42d4cd
limengmeng <limengmeng004@ke.com>
2018-11-09 09:52:10 +0800

    创建实例，一次插入多条数据

--- django_env/env/views.py
+++ django_env/env/views.py
@@ -10,13 +10,21 @@ from common.logger import Log
 # Create your views here.
 
 def testdb(request):
-    list1 = ResourceBase.objects.all()
     resource_base = ResourceBase(base_type='redis', resource_data='i am resource_data', status=1, creator='lmm', is_valid=1)
     resource_base.save()
+
+    resource_base = ResourceBase(base_type='db', resource_data='i am resource_db', status=1, creator='lmm',
+                                 is_valid=1)
+    resource_base.save()
+
+    resource_base = ResourceBase(base_type='kafka', resource_data='i am resource_kafka', status=1, creator='lmm',
+                                 is_valid=1)
+    resource_base.save()
+
+    Log.info("resource_base = ResourceBase(base_type='redis', resource_data='i am resource_data', status=1, creator='lmm', is_valid=1)")
     Log.info(type(resource_base))
     Log.info("resource_base.save()")
-    Log.info("resource_base = ResourceBase(base_type='redis')")
-    return HttpResponse("向数据库ResourceBase表中插入一条数据")
+    return HttpResponse("向数据库ResourceBase表中插入3条数据")
 
 
 
