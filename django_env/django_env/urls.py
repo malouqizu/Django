492dc905ce2c4c9cc00ced5eb8b7e4141643e396
limengmeng <limengmeng004@ke.com>
2019-01-06 07:55:46 +0800

    0106

--- django_env/django_env/urls.py
+++ django_env/django_env/urls.py
@@ -18,11 +18,11 @@ from env import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^testdb_add_method', views.testdb_add_method),
-    url(r'^testdb_update_method', views.testdb_update_method),
-    url(r'^testdb_query_method', views.testdb_query_method),
-    url(r'^testdb_delete_method', views.testdb_delete_method),
-    url(r'^hello', views.hello),
+    url(r'^testdb_add_method/$', views.testdb_add_method),
+    url(r'^testdb_update_method/$', views.testdb_update_method),
+    url(r'^testdb_query_method/$', views.testdb_query_method),
+    url(r'^testdb_delete_method/$', views.testdb_delete_method),
+    url(r'^hello/$', views.hello),
     url(r'^add/$', views.add,name='add'),
     url(r'^add/(\d+)/(\d+)/$', views.add2,name='add2')
 ]
